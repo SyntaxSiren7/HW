ca48426124c084ac0de95f63bbff206a073d3e75
SyntaxSiren7 <bella140397@gmail.com>
2024-07-15 12:23:33 +0300

    modified:   bmi.js

--- bmi.js
+++ bmi.js
@@ -1,6 +1,17 @@
-let height = 160;
+let height = 1.70;
 let weight = 50;
 let bmi = (weight / Math.pow(height, 2))
 
-
-console.log(bmi)
+if (bmi < 16) {
+    console.log(`Severe underweight`)
+} else if (bmi < 16.99) {
+    console.log(`Average underweight`)
+} else if (bmi < 18.49) {
+    console.log(`Slight underweight`)
+} else if (bmi < 24.99) {
+    console.log(`Normal weight`)
+} else if (bmi < 29.99) {
+    console.log(`Preobesity`)
+} else {
+    console.log(`Overweight`)
+}
